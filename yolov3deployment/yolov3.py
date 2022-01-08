e5105dce1a0268d99183e5e318031e335346e651
AnirudhJM24 <84384057+AnirudhJM24@users.noreply.github.com>
2022-01-08 12:49:59 +0530

    Add files via upload

---
+++ yolov3deployment/yolov3.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  8 10:59:28 2022
+
+@author: anirudh
+"""
+import numpy as np
+
+import time
+import cv2
+import os
+
+
+
+import io
+
+from PIL import Image
+
+
+confthres = 0.3
+nmsthres = 0.1
+yolo_path = './'
+
+
+
+
+
+
+def get_labels(labels_path):
+    lpath=os.path.sep.join([yolo_path, labels_path])
+    LABELS = open(lpath).read().strip().split("\n")
+    return LABELS
+
+
+def get_weights(weights_path):
+    weightsPath = os.path.sep.join([yolo_path, weights_path])
+    return weightsPath
+
+def get_config(config_path):
+    configPath = os.path.sep.join([yolo_path, config_path])
+    return configPath
+
+def load_model(configpath,weightspath):
+    print("loading yolo")
+    net = cv2.dnn.readNetFromDarknet(configpath, weightspath)
+    return net
+
+
+
+
+
+def get_predection(image,net,LABELS,COLORS):
+    (H, W) = image.shape[:2]
+
+    ln = net.getLayerNames()
+    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+
+    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
+                                 swapRB=True, crop=False)
+    net.setInput(blob)
+    start = time.time()
+    layerOutputs = net.forward(ln)
+    print(layerOutputs)
+    end = time.time()
+
+    # show timing information on YOLO
+    print("[INFO] YOLO took {:.6f} seconds".format(end - start))
+
+    # initialize our lists of detected bounding boxes, confidences, and
+    # class IDs, respectively
+    boxes = []
+    confidences = []
+    classIDs = []
+
+    # loop over each of the layer outputs
+    for output in layerOutputs:
+        # loop over each of the detections
+        for detection in output:
+            # extract the class ID and confidence (i.e., probability) of
+            # the current object detection
+            scores = detection[5:]
+            # print(scores)
+            classID = np.argmax(scores)
+            # print(classID)
+            confidence = scores[classID]
+
+            # filter out weak predictions by ensuring the detected
+            # probability is greater than the minimum probability
+            if confidence > confthres:
+                # scale the bounding box coordinates back relative to the
+                # size of the image, keeping in mind that YOLO actually
+                # returns the center (x, y)-coordinates of the bounding
+                # box followed by the boxes' width and height
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+
+                # use the center (x, y)-coordinates to derive the top and
+                # and left corner of the bounding box
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+
+                # update our list of bounding box coordinates, confidences,
+                # and class IDs
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                classIDs.append(classID)
+
+    # apply non-maxima suppression to suppress weak, overlapping bounding
+    # boxes
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, confthres,
+                            nmsthres)
+
+    # ensure at least one detection exists
+    if len(idxs) > 0:
+        # loop over the indexes we are keeping
+        for i in idxs.flatten():
+            # extract the bounding box coordinates
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+            
+            c_i = image[y:y+h , x:x+w]
+            cv2.imwrite(LABELS[classIDs[i]]+'.jpg',c_i)
+
+
+
+labelsPath="yolo/classes.names"
+cfgpath="yolo/yolov3_custom.cfg"
+wpath="yolo/yolov3_custom_last.weights"
+Lables=get_labels(labelsPath)
+CFG=get_config(cfgpath)
+Weights=get_weights(wpath)
+nets=load_model(CFG,Weights)
+
+
+def main():
+    image = cv2.imread("./14.jpg")
+    npimg=np.array(image)
+    image=npimg.copy()
+    image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    get_predection(image,nets,Lables,Colors)
+    
+    
+if __name__ == '__main__':
+    main()
