e0445a44d8b7b2d8b7fccaa8351b0d4579a7a9c7
Anirudh JM <84384057+AnirudhJM24@users.noreply.github.com>
2022-06-19 13:06:46 +0530

    "yolov5"

--- model.py
+++ model.py
@@ -1,20 +1,9 @@
 import cv2
 import torch
-
-
-class GetModel:
-
-    def __init__(self, weights_path):
-        print("loading model........")
-        self.weights_path = weights_path
-        self.load_model()
-
-    def load_model(self):
-        detector = torch.hub.load('ultralytics/yolov5', 'custom', path=self.weights_path)
-        return detector
-    # def predict(self,image):
-
+import pandas
 
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='weights/best.pt')
-results = model('examples/14.jpg')
+results = model('examples/sample2_invoice1.jpg')
 crops = results.crop(save=True)
+
+results.pandas().xyxy[0]
