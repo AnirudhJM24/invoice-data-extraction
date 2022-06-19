9df0dfae00eaf5ebf46c8acfb370cc57489fd12e
Anirudh JM <84384057+AnirudhJM24@users.noreply.github.com>
2022-06-19 19:36:23 +0530

    "yolov5"

--- model.py
+++ model.py
@@ -1,9 +1,29 @@
 import cv2
 import torch
 import pandas
+from pdf2image import convert_from_path
+import os
+import shutil
+
+save_dir = 'runs/detect'
+
+os.chmod(save_dir, 0o777)
+
+
+for f in os.listdir(save_dir):
+    shutil.rmtree(os.path.join(save_dir,f))
+
+
+def predict(image):
+    results = model(image)
+    crops = results.crop()
+    print(results.pandas().xyxy[0])
+
 
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='weights/best.pt')
-results = model('examples/sample2_invoice1.jpg')
-crops = results.crop(save=True)
+invoice = 'examples/sample2_invoice1.jpg'  # path to image
+img_supp_types = '.jpg'
+if invoice.endswith(img_supp_types):
+    predict(invoice)
 
-results.pandas().xyxy[0]
+
