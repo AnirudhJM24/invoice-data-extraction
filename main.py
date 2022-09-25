c840d170d741d1f4588a0b50bf9919ea88e91cdf
Abhay H Kashyap <abhayastro1004@gmail.com>
2022-09-25 17:59:31 +0530

    Added Retraining

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import cv2
-import torch
-import pandas
 from pdf2image import convert_from_path
 import os
-import shutil
-import sys
-import pytesseract
-from PIL import Image
 import argparse
 
 from preprocess import preimgpdf
@@ -19,7 +12,7 @@ warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 
 
 parser = argparse.ArgumentParser(description='run invoice detection')
-parser.add_argument('--file',help = 'enter path of invoice')
+parser.add_argument('--file', help='enter path of invoice')
 args = parser.parse_args()
 
 
@@ -29,31 +22,33 @@ def main():
     ocr = text_extract()
     invoice_path = args.file
     invoice = invoice_path
-      # path to image
+    # path to image
     img_supp_types = '.jpg' or '.png'
 
     if invoice.endswith(img_supp_types):
         detector.predict(invoice_path)
-        crop_img_paths, table_dir = preprocess.set_dir(invoice,1)
+        crop_img_paths, table_dir = preprocess.set_dir(invoice, 1)
         for img in crop_img_paths:
-            ocr.retrieve_text(img['field'],img['path'])
+            ocr.retrieve_text(img['field'], img['path'])
         print('TABLE DETAILS:')
         table_path = table_dir
-        os.system('python table-extraction\\table_transformer.py --table-type borderless -i ' + table_path)
+        os.system(
+            'python table-extraction\\table_transformer.py --table-type borderless -i ' + table_path)
         #print(crop_img_paths, table_dir)
-
 
     elif invoice.endswith('.pdf'):
         images = preprocess.pdf_images(invoice)
-        for i,imag in enumerate(images):
+        for i, imag in enumerate(images):
             detector.predict(imag)
-            crop_img_paths, table_dir = preprocess.set_dir(imag,i+1)
+            crop_img_paths, table_dir = preprocess.set_dir(imag, i+1)
             for img in crop_img_paths:
-                ocr.retrieve_text(img['field'],img['path'])
+                ocr.retrieve_text(img['field'], img['path'])
             table_path = table_dir
             try:
-                os.system('python table-extraction\\table_transformer.py --table-type borderless -i ' + table_path)
-                os.system('python table-extraction\\table_transformer.py --table-type bordered -i ' + table_path)
+                os.system(
+                    'python table-extraction\\table_transformer.py --table-type borderless -i ' + table_path)
+                os.system(
+                    'python table-extraction\\table_transformer.py --table-type bordered -i ' + table_path)
             except:
                 print("no table")
 
--- yolov.py
+++ yolov.py
@@ -1,23 +1,12 @@
-import cv2
 import torch
-import pandas
-from pdf2image import convert_from_path
-import os
-import shutil
-import sys
-import pytesseract
-from PIL import Image
-import argparse
-
-from preprocess import preimgpdf
 
 
 class model:
 
-    def __init__(self,model_path):
-        self.model = torch.hub.load('ultralytics/yolov5','custom', path = model_path)
-    
+    def __init__(self, model_path):
+        self.model = torch.hub.load(
+            'ultralytics/yolov5', 'custom', path=model_path)
+
     def predict(self, image):
         results = self.model(image)
         results.crop()
-
